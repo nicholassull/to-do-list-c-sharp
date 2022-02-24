5efd933f2c3601f31bca64649633545aae38fab5
Nick Sullivan <nicholassull@gmail.com>
2022-02-23 17:53:01 -0800

    add GetAll, ClearAll, and Find method to Category

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -8,5 +8,27 @@ namespace ToDoList.Models
     public string Name { get; set; }
     public int Id { get; }
     public List<Item> Items { get; set; }
+
+    public Category(string categoryName)
+    {
+      Name = categoryName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Items = new List<Item>{};
+    }
+
+    public static List<Category> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static Category Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
   }
 }
