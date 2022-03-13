470c75bb5c7c8ed2e0b02841c763dbe945c2a842
Nick Sullivan <nicholassull@gmail.com>
2022-03-13 12:41:27 -0700

    add many-many functionality to Category views and controller

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -49,7 +49,7 @@ namespace ToDoList.Controllers
     public ActionResult Edit (int id)
     {
       var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-      ViewBag.CategoryId = new SelectList(_db.Categories, "CuisineId", "Name");
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View (thisItem);
     }
     [HttpPost]
